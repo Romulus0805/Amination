098c9fb516f08b889924d0a4ebfbd8674ba9cedc
Romulus0805 <emb_14@icloud.com>
2026-06-10 13:54:59 -0700

    Add files via upload

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Mobile on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
